599bdddd2e5098ccaf63ed567477585191a293fc
Ali El-Ashram <ali.elashram@gmail.com>
2013-10-10 09:58:31 -0700

    Refactor edit a post test.

--- test/features/editing_a_post_test.rb
+++ test/features/editing_a_post_test.rb
@@ -3,17 +3,14 @@ require "test_helper"
 feature "Editing a Post" do
   scenario "submit updates to an existing post" do
 
-    # A post will be created
-    @post = posts(:cf)
-    # The post author goes to that post's show page
-    visit post_path(@post)
+    # The post author goes to a post's show page
+    visit post_path(posts(:cf))
 
     # That page should have a link to "Edit" that the author can click
     click_on 'Edit'
 
     # A form is filled in with the changed attributes
     page.text.must_include "Means striving for excellence."
-
 
     # The form is submitted
     click_on 'Update Post'
