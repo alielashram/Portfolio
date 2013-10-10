4bd13b2e5edc84582745be22ab59b6770b6e1124
Ali El-Ashram <ali.elashram@gmail.com>
2013-10-10 11:52:53 -0700

    Drop users table from schema.rb

--- db/migrate/20131010182222_drop.rb
+++
@@ -1,7 +0,0 @@
-class Drop < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131010182222) do
+ActiveRecord::Schema.define(:version => 20131010185056) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
