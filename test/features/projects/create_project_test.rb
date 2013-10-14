c2ba17b9a91aa43bec9790d1f3a98c2460cd78d9
Ali El-Ashram <ali.elashram@gmail.com>
2013-10-14 16:17:34 -0700

    Add a test for an unhappy path. Fixes #24

---
+++ test/features/projects/create_project_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+feature "As the site owner, I want to add a portfolio item so that I can show off my work" do
+  scenario "adding a new project" do
+    visit projects_path
+    click_on "New project"
+    fill_in "Name", with: projects(:ae).name
+    fill_in "Technologies used", with: projects(:ae).technologies_used
+    click_on "Create Project"
+    page.text.must_include "Project has been created."
+    page.text.must_include "Rails"
+  end
+
+  scenario "new project has invalid data" do
+    # Given invalid project data is entered in a form
+    visit new_project_path
+    fill_in "Name", with: "Q"
+
+    # When the form is submitted with a short name and missing technologies_used field
+    save_and_open_page
+    click_on "Create Project"
+
+
+    # Then the form should be displayed again, with an error message
+    current_path.must_match /projects$/
+    page.text.must_include "Project could not be saved"
+    page.text.must_include "Name is too short"
+    page.text.must_include "Technologies used can't be blank"
+  end
+end
