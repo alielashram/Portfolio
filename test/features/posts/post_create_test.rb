8ec508906e5929e6c9166891153a25f9c6561af2
Ali El-Ashram <ali.elashram@gmail.com>
2013-10-15 19:22:04 -0700

    Update site css.

--- test/features/posts/post_create_test.rb
+++ test/features/posts/post_create_test.rb
@@ -3,18 +3,28 @@ require "test_helper"
 feature "creating a post" do
   scenario "works with valid data" do
 
-    # Given a completed post form
-    visit posts_path
-    click_on 'New Post'
-    fill_in 'Title', with: posts(:cf).title
-    fill_in 'Content', with: posts(:cf).content
+    # Given an authorized user complets a new post form
+    visit new_user_session_path
+    fill_in "Email", with: users(:df).email
+    fill_in "Password", with: users(:df).encrypted_password
+    click_on "Sign in"
 
-    # When I submit the form
+    visit posts_path
+
+    click_on "New Post"
+
+    fill_in "Title", with: posts(:cf).title
+    fill_in "Content", with: posts(:cf).content
     click_on 'Create Post'
 
     # A new post should be created and displayed
     page.text.must_include 'Post was successfully created'
     page.text.must_include posts(:cf).title
+
+    # When I submit the form
+
+
+
 
     # New post should be attributed to a user
     page.has_css? "#author"
